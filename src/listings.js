886c6ba06b68b36d4383adea6f4a37eda930eab7
mjbuchman <mbookie213@gmail.com>
2019-10-31 20:40:57 -0500

    filters in

--- src/listings.js
+++ src/listings.js
@@ -3,44 +3,62 @@ import logo from './logo.svg';
 import './css/listings.css';
 import Slideshow from './components/slideshow.js';
 import Footer from './components/footer.js';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Listings extends React.Component {
+  opt = [ "One", "Two", "Three"];
+
   render() {
     return (
       <div className="page">
+        <div className="filters">
+          <Dropdown options={this.opt} className='myClassName' />
+          <Dropdown options={this.opt} className='myClassName' />
+          <Dropdown options={this.opt} className='myClassName' />
+        </div>
+
+        <hr/>
+
         <div className="grid-container">
           <div className="card">
             <Slideshow/>
             <div class="container">
-              <h4><b>John Doe</b></h4>
-              <p>Architect and Engineer</p>
+              <h4><b>[TYPE]</b></h4>
+              <p>[LOCATION]</p>
+              <p>[COST]</p>
             </div>
           </div>
           
           <div className="card">
             <Slideshow/>
             <div class="container">
-              <h4><b>John Doe</b></h4>
-              <p>Architect and Engineer</p>
+              <h4><b>[TYPE]</b></h4>
+              <p>[LOCATION]</p>
+              <p>[COST]</p>
             </div>
           </div>
           
           <div className="card">
             <Slideshow/>
             <div class="container">
-              <h4><b>John Doe</b></h4>
-              <p>Architect and Engineer</p>
+              <h4><b>[TYPE]</b></h4>
+              <p>[LOCATION]</p>
+              <p>[COST]</p>
             </div>
           </div>
           
           <div className="card">
             <Slideshow/>
             <div class="container">
-              <h4><b>John Doe</b></h4>
-              <p>Architect and Engineer</p>
+              <h4><b>[TYPE]</b></h4>
+              <p>[LOCATION]</p>
+              <p>[COST]</p>
             </div>
           </div>
+          
+         
         </div>
         <Footer></Footer>
       </div>
