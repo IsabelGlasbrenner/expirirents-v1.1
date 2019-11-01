497eaebb28d033438c933f7e94d1a1a990395528
DESKTOP-3RMTD42\isabe <isabel.glasbrenner@gmail.com>
2019-11-01 16:25:37 -0500

    Adding landing page and header component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './css/App.css';
+import React from "react";
+import mountains from "./images/mountains.jpg";
+import "./css/App.css";
+import Footer from "./components/footer.js";
+import Header from "./components/header.js";
+import adventure_text from "./images/adventure_text.png";
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={mountains} className="Mountains" alt="Mountains" />
+          <h2 className="heading">
+            Check out the ATV's for rent in your area...
+          </h2>
+          <div className="searchBox">
+            <label className="input">Search</label>
+            <input type="text" className="input" />
+          </div>
         </header>
+        <Footer />
       </div>
     );
   }
