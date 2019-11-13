0a87e1d0c53684b9c126a5192839c27f04a710a7
DESKTOP-3RMTD42\isabe <isabel.glasbrenner@gmail.com>
2019-11-13 16:26:19 -0600

    Connect to Backend

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,55 @@
-import React from 'react';
-import './css/footer.css';
-import { SocialIcon } from 'react-social-icons';
+import React from "react";
+import "./css/footer.css";
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
     <footer>
-      <div class="container">
+      <div className="container">
         <div className="menu">
-          <ul class="footer-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Listings</a></li>
-            <li><a href="#">New List</a></li>
+          <ul className="footer-menu">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Listings</a>
+            </li>
+            <li>
+              <a href="#">New List</a>
+            </li>
           </ul>
         </div>
-        <div class="social-icons">
-          <SocialIcon url="http://twitter.com/" fgColor="#FFFFFF" style={{ height: 35, width: 35}}/> 
-          <SocialIcon url="http://facebook.com/" fgColor="#FFFFFF" style={{ height: 35, width: 35}}/> 
-          <SocialIcon url="http://instagram.com/" fgColor="#FFFFFF" style={{ height: 35, width: 35}}/> 
-          <SocialIcon url="http://yelp.com/" fgColor="#FFFFFF" style={{ height: 35, width: 35}}/> 
-          <SocialIcon url="http://pinterest.com/" fgColor="#FFFFFF" style={{ height: 35, width: 35}}/> 
+        <div className="social-icons">
+          <SocialIcon
+            url="http://twitter.com/"
+            fgColor="#FFFFFF"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="http://facebook.com/"
+            fgColor="#FFFFFF"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="http://instagram.com/"
+            fgColor="#FFFFFF"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="http://yelp.com/"
+            fgColor="#FFFFFF"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="http://pinterest.com/"
+            fgColor="#FFFFFF"
+            style={{ height: 35, width: 35 }}
+          />
         </div>
         <p>© Copyright 2019 Expirirents - All Rights Reserved</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/login-or-signup.js
+++ src/login-or-signup.js
@@ -4,23 +4,83 @@ import Header from "./components/header.js";
 import Footer from "./components/footer.js";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const initialState = {
+  email: "",
+  password: ""
+};
+
 class LoginOrSignup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  handleChange = async event => {
+    console.log("handle change called /n");
+    const { name, value } = event.target;
+    await this.setState({ [name]: value });
+  };
+
+  handleLogin = async event => {
+    event.preventDefault();
+
+    if (this.state.email === "" || this.state.password === "")
+      alert("Enter Login Credentials");
+    fetch("http://18.224.3.21/user/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.id) {
+          this.props.history.push("/", { email: this.state.email });
+        } else {
+          alert("Email and password do not match. Please try again!");
+        }
+      });
+  };
+
   render() {
     return (
       <div className="parent">
         <Header />
         <div className="body">
           <h2 className="heading">Login or Signup to get started.</h2>
-          <div className="login">
+
+          <form className="login" onSubmit={this.handleLogin}>
             <div className="Username">
-              <label className="label">UserName:</label>
-              <input type="text" className="input" />
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="Email address"
+                onChange={this.handleChange}
+                className="input"
+              />
             </div>
             <div className="Password">
-              <label className="label">Password:</label>
-              <input type="text" className="input" />
+              <input
+                type="Password"
+                name="password"
+                required
+                placeholder="Password"
+                onChange={this.handleChange}
+                className="input"
+              />
             </div>
-          </div>
+            <div>
+              <button className="password-submit" onClick={this.handleLogin}>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
         <Footer />
       </div>
