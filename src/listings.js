ca62f1a2d0db26d068887ae8a4c8a3f25e68d944
mjbuchman <mbookie213@gmail.com>
2019-11-15 13:29:36 -0600

    fetch set up - wrong data

--- src/listings.js
+++ src/listings.js
@@ -5,14 +5,35 @@ import Footer from './components/footer.js';
 import Header from './components/header.js';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
+
+const opt = [ "One", "Two", "Three"];
+const initialState = {
+	data: {}
+}
 
 class Listings extends React.Component {
-  opt = [ "One", "Two", "Three"];
 
-  render() {
-    return (
-      <div className="parent">
+	constructor(props) {
+		super(props);
+		this.state = initialState;
+	}
+	
+	componentDidMount() {
+		fetch("http://18.224.3.21/user/listing")
+		.then(res => {
+			console.log(res);
+			return res.json()
+		})
+		.then(data => {
+			this.setState({data: data});
+		})
+		console.log(this.state.data)
+	}	
+
+	render() {
+	return (
+		<div className="parent">
         <Header />
         <div className="page">
           <div className="filters">
@@ -27,51 +48,28 @@ class Listings extends React.Component {
             <h2>Find your next rental</h2>
             <h4>Explore hundreds of listings</h4>
           </div>
-        
-          <div className="grid-container">
-            <div className="card">
-              <Slideshow/>
-              <Link to="/single-listing">
-                <div className="container">
-                  <p><b>[TYPE]</b></p>
-                  <p>[LOCATION]</p>
-                  <p>[COST]</p>
-                </div>
-              </Link>
-            </div>
-            
-            <div className="card">
-              <Slideshow/>
-              <div class="container">
-                <p><b>[TYPE]</b></p>
-                <p>[LOCATION]</p>
-                <p>[COST]</p>
-              </div>
-            </div>
-            
-            <div className="card">
-              <Slideshow/>
-              <div class="container">
-                <p><b>[TYPE]</b></p>
-                <p>[LOCATION]</p>
-                <p>[COST]</p>
-              </div>
-            </div>
-            
-            <div className="card">
-              <Slideshow/>
-              <div class="container">
-                <p><b>[TYPE]</b></p>
-                <p>[LOCATION]</p>
-                <p>[COST]</p>
-              </div>
-            </div>
-            
-          
-          </div>
+              
+			<div className="grid-container"> {
+				// this.state.data.map((listings, i) => {
+				// 	return (
+				// 		<div key={i} className="card">
+				// 			<Slideshow/>
+				// 			<Link to="/single-listing">
+				// 			<div className="container">
+				// 				<p><b>[TYPE]</b></p>
+				// 				<p>[LOCATION]</p>
+				// 				<p>[COST]</p>
+				// 			</div>
+				// 			</Link>
+				// 		</div>
+				// 	);
+				// })
+			}
+
         </div>
         <Footer />
       </div>
+	  </div>
     );
   }
 }
