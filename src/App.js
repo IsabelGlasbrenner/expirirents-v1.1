09c57d3ef5caea02e0c3e593dd5b5b642fec96d1
khanikar <neeshankhanikar@gmail.com>
2019-11-15 17:31:51 -0600

    fixed calendar issue and centered text. Need to save calendar options on change.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import mountains from "./images/mountains.jpg";
 import "./css/App.css";
 import Footer from "./components/footer.js";
 import Header from "./components/header.js";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import adventure_text from "./images/adventure_text.png";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
+
+  opt = ["Motorcycle", "Dirt Bike", "Mountain Bike", "ATV", "Boat"];
+  state = {
+    stateDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -20,6 +36,28 @@ class App extends React.Component {
           <div className="searchBox">
             <label className="input">Location</label>
             <input type="text" className="input" />
+            <label className="title">
+                Location:
+              </label>
+              <input type="text" className="input" />
+              <Dropdown options={this.opt} className="filters"/>
+              <label className="title">
+                From: 
+              </label>
+              <DatePicker
+                selected={this.state.startDate}
+                onSelect={this.handleSelect}
+                onChange={this.handleChange}
+              />
+              <label className="title">
+                To:
+              </label>
+              <DatePicker
+                selected={this.state.startDate}
+                onSelect={this.handleSelect}
+                onChange={this.handleChange}
+              />
+              <input type="submit" value="Search" />
           </div>
         </header>
         <Footer />
