a0b029eff9c92c45a87ebede27b22b3e46af8825
DESKTOP-3RMTD42\isabe <isabel.glasbrenner@gmail.com>
2019-11-15 14:31:16 -0600

    Prettier header

--- src/components/header.js
+++ src/components/header.js
@@ -4,16 +4,15 @@ import logo from "../images/logo.jpg";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <img src={logo} className="logo" alt="expiriRents" />
-      <ul>
-        <Link to="/"><li>Home</li></Link>
-        <Link to="/listings"><li>All Listings</li></Link>
-        <Link to="/login-or-signup"><li>Login/Signup</li></Link>
-      </ul>
-    </header>
-  );
+	return (
+		<header className="header">
+			<Link to="/"><img src={logo} className="logo" alt="expiriRents" /></Link>
+			<ul>
+				<Link to="/listings"><li>All Listings</li></Link>
+				<Link to="/login-or-signup"><li>Login/Signup</li></Link>
+			</ul>
+		</header>
+	);
 };
 
 export default Header;
