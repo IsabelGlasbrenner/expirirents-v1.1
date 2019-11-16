f9e263c5c5310e5bf29b574a124a4f63b0893d64
khanikar <neeshankhanikar@gmail.com>
2019-11-16 11:28:35 -0600

    some css stuff

--- src/App.js
+++ src/App.js
@@ -93,6 +93,25 @@ class App extends React.Component {
               modifiers={modifiers}
               onDayClick={this.handleDayClick}
             />
+            <Helmet>
+              <style>{`
+  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+    background-color: #f0f8ff !important;
+    color: #4a90e2;
+  }
+  .Selectable .DayPicker-Day {
+    border-radius: 0 !important;
+  }
+  .Selectable .DayPicker-Day--start {
+    border-top-left-radius: 50% !important;
+    border-bottom-left-radius: 50% !important;
+  }
+  .Selectable .DayPicker-Day--end {
+    border-top-right-radius: 50% !important;
+    border-bottom-right-radius: 50% !important;
+  }
+`}</style>
+            </Helmet>
             {/* <label className="title">
               From:
               </label>
